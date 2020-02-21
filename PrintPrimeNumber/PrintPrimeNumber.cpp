2fa9b56451ced3de7214aa2e1a7f90ec0ad54c24
FLH0202 <FLH0202@163.com>
2020-02-21 21:06:23 +0800

    删除最后多余的注释

--- PrintPrimeNumber/PrintPrimeNumber.cpp
+++ PrintPrimeNumber/PrintPrimeNumber.cpp
@@ -2,19 +2,49 @@
 //
 
 #include <iostream>
+//#include"stdlib.h"
+//#include"stdafx.h"
+
+using namespace std;
+
+//判断输入的数字是否为素数
+//返回值0：非素数
+//返回值1：素数
+int checkPrimeNumber(int n) {
+
+	int a = 0;//用于计算输入数字可以被整除的数字的个数
+	for (int i = 2; i < n - 1; i++) {
+		if ((n%i) == 0) {
+			a++;
+		}
+		else {
+			a = a;
+		}
+	}
+	if (a == 0) {
+
+		return 1;
+	}
+	else {
+		return 0;
+
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int Number = 100;
+	for (int k = 2; k < Number; k++) {
+
+		//判断数字
+		int ans = checkPrimeNumber(k);
+		if (ans == 1) {
+			cout << "the number is:" << k << endl;
+		}
+	}
+	system("pause");
+    
+	return 0;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
