4ae0460f0d59fe57a79dbb4e9ebd1f5eb822f80b
FLH0202 <FLH0202@163.com>
2020-02-21 21:20:02 +0800

    终版

--- PrintPrimeNumber/PrintPrimeNumber.cpp
+++ PrintPrimeNumber/PrintPrimeNumber.cpp
@@ -1,15 +1,10 @@
-﻿// PrintPrimeNumber.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿
 
 #include <iostream>
 //#include"stdlib.h"
 //#include"stdafx.h"
 
 using namespace std;
-
-int checkTest(int m) {
-
-}
 
 //判断输入的数字是否为素数
 //返回值0：非素数
